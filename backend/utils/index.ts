0d3f8113a88f8b8b5965c6d19b8ff5759a14057e
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-04-28 17:44:29 -0300

    stop over engineering

--- backend/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import { createHash } from "node:crypto"
-
-export * from "./mysql"
-
-export function hashPassword(password: string): string {
-    return createHash("sha256").update(password).digest("hex")
-}
